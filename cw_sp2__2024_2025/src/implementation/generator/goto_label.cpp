1065fd1865e063c95a62f8f722c4e2e81c12c4d8
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-01-06 11:34:21 +0200

    Add files via upload

--- cw_sp2__2024_2025/src/implementation/generator/goto_label.cpp
+++ cw_sp2__2024_2025/src/implementation/generator/goto_label.cpp
@@ -16,18 +16,18 @@
 std::map<std::string, unsigned long long int> labelInfoTable;
 
 unsigned char* makeLabelCode(struct LexemInfo** lastLexemInfoInTable, unsigned char* currBytePtr, unsigned char generatorMode) {
-	unsigned char multitokenSize = detectMultiToken(*lastLexemInfoInTable + 1, MULTI_TOKEN_NULL_STATEMENT);
-	if ((*lastLexemInfoInTable)->tokenType != IDENTIFIER_LEXEME_TYPE
-		|| !(multitokenSize += detectMultiToken(*lastLexemInfoInTable + multitokenSize + 1, MULTI_TOKEN_COLON))) {
+	unsigned char multitokenSize, multitokenSize_ = detectMultiToken(*lastLexemInfoInTable + 1, MULTI_TOKEN_NULL_STATEMENT);
+	multitokenSize = detectMultiToken(*lastLexemInfoInTable + multitokenSize_ + 1, MULTI_TOKEN_COLON);
+	if (multitokenSize) {
+		multitokenSize += multitokenSize_;
+	}
+	if ((*lastLexemInfoInTable)->tokenType != IDENTIFIER_LEXEME_TYPE){
 		return currBytePtr;
 	}
-	else {
-		++multitokenSize;
-	}
-	if (multitokenSize) {
+	if (multitokenSize++) {
 #ifdef DEBUG_MODE_BY_ASSEMBLY
 		printf("\r\n");
-		printf("    ;ident \"%s\" previous \"%s\" (as label)\r\n", (*lastLexemInfoInTable)->lexemStr, tokenStruct[MULTI_TOKEN_COLON][0]);
+		printf("    ;ident \"%s\"(as label) previous \"%s\"\r\n", (*lastLexemInfoInTable)->lexemStr, tokenStruct[MULTI_TOKEN_COLON][0]);
 #endif
 
 		labelInfoTable[(*lastLexemInfoInTable)->lexemStr] = (unsigned long long int)currBytePtr;
@@ -44,19 +44,23 @@ unsigned char* makeLabelCode(struct LexemInfo** lastLexemInfoInTable, unsigned c
 
 unsigned char* makeGotoLabelCode(struct LexemInfo** lastLexemInfoInTable, unsigned char* currBytePtr, unsigned char generatorMode) {
 	unsigned char multitokenSize = detectMultiToken(*lastLexemInfoInTable, MULTI_TOKEN_GOTO);
-	if (multitokenSize) {
+	if (multitokenSize++) {
 		if ((*lastLexemInfoInTable + 1)->tokenType != IDENTIFIER_LEXEME_TYPE) {
 			return currBytePtr;
 		}
 #ifdef DEBUG_MODE_BY_ASSEMBLY
 		printf("\r\n");
-		printf("    ;\"%s\" (after %s)\r\n", (*lastLexemInfoInTable)->lexemStr, tokenStruct[MULTI_TOKEN_GOTO][0]);
+		printf("    ;\"%s\" previous ident \"%s\"(as label)\r\n", tokenStruct[MULTI_TOKEN_GOTO][0], (*lastLexemInfoInTable)[1].lexemStr);
 #endif
 
 		const unsigned char code__jmp_offset[] = { 0xE9, 0x00, 0x00, 0x00, 0x00 };
 
 		currBytePtr = outBytes2Code(currBytePtr, (unsigned char*)code__jmp_offset, 5);
 		*(unsigned int*)(currBytePtr - 4) = (unsigned int)((unsigned char*)labelInfoTable[(*lastLexemInfoInTable)->lexemStr] - currBytePtr);
+
+#ifdef DEBUG_MODE_BY_ASSEMBLY
+		printf("    jmp LABEL@%016llX:\r\n", (unsigned long long int)lexemInfoTransformationTempStack[lexemInfoTransformationTempStackSize - 1].lexemStr);
+#endif
 
 		return *lastLexemInfoInTable += multitokenSize, currBytePtr;
 	}
