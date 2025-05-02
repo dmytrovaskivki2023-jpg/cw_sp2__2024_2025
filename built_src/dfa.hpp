aef9f0de5b219bfc72125b0367fc20753ee462f2
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-05-02 04:30:10 +0300

    Add files via upload

---
+++ built_src/dfa.hpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+* N.Kozak // Lviv'2020 // ACM // DFA for input string example (method 1) *
+*    file: acmhw1_m1.c                                                   *
+**************************************************************************/
+#include "stdio.h"
+
+#define SYMBOL_NUMBER 256
+#define MAX_STATES 1024
+
+typedef int INSTRUCTION;
+typedef INSTRUCTION PROGRAM[SYMBOL_NUMBER][MAX_STATES];
+
+typedef struct StructDFA{
+	char * data;
+	PROGRAM * program;
+	void(*run)(struct StructDFA * dfa);
+	int state;
+} DFA;
+
+void runner(DFA * dfa){
+	for (; *dfa->data != '\0'; ++dfa->data){
+		dfa->state = (*dfa->program)[*dfa->data][dfa->state];
+	}
+}
+
+char tryToAccept(PROGRAM * program, int * finitStates, char * data){
+	DFA dfa = { data, program, runner, 0/*Q000*/ };
+	dfa.run(&dfa);
+
+	for (; *finitStates; ++finitStates) {
+		if (dfa.state == *finitStates) {
+			printf("DFA: finit state\r\n");
+			return 1;
+		}
+	}
+
+	printf("DFA: no finit state\r\n");
+	return 0;
+}
