7e9eabf29b2a376d0c527394bf9d437bd457c835
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-10-10 02:13:54 +0300

    Add files via upload

--- verify_syntax_by_EBNF_nonpacked__2025/main.cpp
+++ verify_syntax_by_EBNF_nonpacked__2025/main.cpp
@@ -44,7 +44,7 @@ struct cwgrammar : qi::grammar<Iterator> {
         //
         unary_operator = SAME_RULE(tokenNOT);
         unary_operation = unary_operator >> expression;
-        binary_operator = tokenAND | tokenOR | tokenEQUAL | tokenNOTEQUAL | tokenLESSOREQUAL | tokenGREATEROREQUAL | tokenPLUS | tokenMINUS | tokenMUL | tokenDIV | tokenMOD;
+        binary_operator = tokenAND | tokenOR | tokenEQUAL | tokenNOTEQUAL | tokenLESS | tokenGREATER | tokenLESSOREQUAL | tokenGREATEROREQUAL | tokenPLUS | tokenMINUS | tokenMUL | tokenDIV | tokenMOD;
         binary_action = binary_operator >> expression;
         //
         left_expression = group_expression | unary_operation | ident >> -index_action | value | cond_block__with_optionally_return_value;
